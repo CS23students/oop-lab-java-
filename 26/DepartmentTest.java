f3214956cdf91e993030a1881166bd6c71351e3c
CS23students <153345000+CS23students@users.noreply.github.com>
2024-11-28 13:35:25 +0530

    Update DepartmentTest.java

--- 26/DepartmentTest.java
+++ 26/DepartmentTest.java
@@ -1,3 +1,5 @@
+//26. ᴄʀᴇᴀᴛᴇ ᴩᴀᴄᴋᴀɢᴇ ᴄᴀʟʟᴇᴅ ᴅᴇᴩᴛ.ꜰᴏᴜʀ ᴄʟᴀꜱꜱ ᴄʀᴇᴀᴛᴇ ᴄꜱᴇ,ᴇᴄᴇ,ᴇᴇᴇ,ᴍᴇ ᴇᴀᴄʜ ᴄʟᴀꜱꜱ ᴡʜɪᴄʜ ꜱʜᴏᴡ ꜱᴜʙᴊᴇᴄᴛ ʀᴇᴩᴇʀᴇꜱᴇɴᴛ yᴇᴀʀ
+
 import dept.CSE;
 import dept.ECE;
 import dept.EEE;
@@ -18,3 +20,22 @@ public class DepartmentTest {
         me.showSubjectAndYear();
     }
 }
+
+
+
+// OUTPUT:
+// Department: Computer Science Engineering (CSE)
+// Subjects: Data Structures, Algorithms, Operating Systems, etc.
+// Year: 1st Year to 4th Year
+
+// Department: Electronics and Communication Engineering (ECE)
+// Subjects: Signals and Systems, Digital Electronics, etc.
+// Year: 1st Year to 4th Year
+
+// Department: Electrical and Electronics Engineering (EEE)
+// Subjects: Circuit Theory, Electrical Machines, Power Systems, etc.
+// Year: 1st Year to 4th Year
+
+// Department: Mechanical Engineering (ME)
+// Subjects: Thermodynamics, Strength of Materials, Fluid Mechanics, etc.
+// Year: 1st Year to 4th Year
