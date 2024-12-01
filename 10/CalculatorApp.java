3dfb501e520f1fa18ac0c771b2131a8fa661ccbc
CS23students <153345000+CS23students@users.noreply.github.com>
2024-12-01 17:26:33 +0530

    Update CalculatorApp.java

--- 10/CalculatorApp.java
+++ 10/CalculatorApp.java
@@ -1,6 +1,14 @@
 // 10.Design a Calculator using event driving programming paradigm of java with decimal manipulation
 
-
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
 
 public class CalculatorApp extends Application {
 
