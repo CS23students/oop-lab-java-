b56997443104c928a92719fbb01349112dfbc8a8
CS23students <153345000+CS23students@users.noreply.github.com>
2024-12-02 12:25:56 +0530

    Update 12-b-max-val-generic.java

--- 12/12-b-max-val-generic.java
+++ 12/12-b-max-val-generic.java
@@ -1,36 +1,39 @@
 //12-b) Write a java program to  find the maximum value of the give data types using generic //function
 
-public class MaximumValueFinder {
+public class GenericMaxFinder {
 
-    // Function to find maximum of three integers
-    public static int findMaximum(int a, int b, int c) {
-        int max = a;
-        if (b > max) {
-            max = b;
-        }
-        if (c > max) {
-            max = c;
-        }
-        return max;
-    }
-
-    // Function to find maximum of three doubles
-    public static double findMaximum(double a, double b, double c) {
-        double max = a;
-        if (b > max) {
-            max = b;
-        }
-        if (c > max) {
-            max = c;
+    // Generic function to find the maximum value
+    public static <T extends Comparable<T>> T findMax(T[] array) {
+        T max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].compareTo(max) > 0) {
+                max = array[i];
+            }
         }
         return max;
     }
 
     public static void main(String[] args) {
-        System.out.println("Maximum of 3, 7, 5 (int): " + findMaximum(3, 7, 5));
-        System.out.println("Maximum of 3.5, 2.1, 4.8 (double): " + findMaximum(3.5, 2.1, 4.8));
+        // Finding maximum for Integer array
+        Integer[] intArray = {10, 25, 3, 50, 7};
+        System.out.println("Maximum Integer value: " + findMax(intArray));
+
+        // Finding maximum for Double array
+        Double[] doubleArray = {2.5, 7.8, 1.3, 9.4, 5.6};
+        System.out.println("Maximum Double value: " + findMax(doubleArray));
+
+        // Finding maximum for String array (based on lexicographical order)
+        String[] stringArray = {"Apple", "Orange", "Banana", "Peach", "Mango"};
+        System.out.println("Maximum String value: " + findMax(stringArray));
     }
 }
+
+
+// OUTPUT:
+// Maximum Integer value: 50
+// Maximum Double value: 9.4
+// Maximum String value: Peach
+
 
 
 // OUTPUT:
